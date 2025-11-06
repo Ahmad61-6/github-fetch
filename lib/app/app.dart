0e2b844723890691bb51082c93c4afa4e5ca5f8c
Ahmad61-6 <ahmadmizan210@gmail.com>
2025-11-06 12:56:30 +0600

    just started

---
+++ lib/app/app.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class GithubFetch extends StatelessWidget {
+  const GithubFetch({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp();
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:github_fetch/app/app.dart';
+
+void main() {
+  runApp(GithubFetch());
+}
